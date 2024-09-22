e75a5353dcfc7ebf0a95f6dec6f270dafda64418
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-09-22 17:16:27 +0500

    Sitemap

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,9 @@
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>2024-09-22T17:16:00+05:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.00</priority>
+    </url>
+</urlset>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteMapGenerator;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', Home::class)->name('home');
 
@@ -15,5 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/sitemap.xml', [SiteMapGenerator::class, 'index'])->name('sitemap');
 
 require __DIR__ . '/auth.php';
