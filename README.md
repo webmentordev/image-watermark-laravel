917bd024ded534865741cafb541861277621a760
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-09-22 16:51:18 +0500

    Message & Data

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # VPS Deployment Steps (DigitalOcean's Droplet)
 
 1. Link Storage
-2. Install imagemagick and php-imagick package in sequence
+2. Install imagemagick and php-imagick package in sequence  
 sudo apt install imagemagick php-imagick
 3. Install Node & NPM on the server
 4. Change ownership & pwemissions
+
 sudo chown -R larasail:larasail /var/www/laravel
+
 sudo chown -R larasail:www-data /var/www/laravel/storage
+
 sudo chown -R larasail:www-data /var/www/laravel/bootstrap/cache
 
-sudo find /var/www/laravel -type d -exec chmod 755 {} \;
-sudo find /var/www/laravel -type f -exec chmod 644 {} \;
-sudo chmod -R 775 /var/www/laravel/storage /var/www/laravel/bootstrap/cache
 
+sudo find /var/www/laravel -type d -exec chmod 755 {} \;
+
+sudo find /var/www/laravel -type f -exec chmod 644 {} \;
+
+sudo chmod -R 775 /var/www/laravel/storage /var/www/laravel/bootstrap/cache  
 **If NPM command cause problem for permissions**
+
 sudo chmod -R 755 /var/www/laravel/node_modules
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,13 @@
 <section class="h-screen flex items-center justify-center">
     <form wire:submit.prevent="addWatermark" enctype="multipart/form-data" class="flex flex-col max-w-2xl w-full">
         @if ($image)
-            <img class="mb-3 rounded-lg overflow-hidden" src="{{ $image->temporaryUrl() }}">
+            <h1 class="text-2xl mb-3 text-white">Preview</h1>
+            <div class="bg-contain bg-center flex items-center justify-center h-[600px] w-full bg-no-repeat"
+                style="background-image: url({{ $image->temporaryUrl() }})">
+                @if ($watermark)
+                    <img src="{{ $watermark->temporaryUrl() }}" width="150px">
+                @endif
+            </div>
         @endif
 
         @session('error')
@@ -9,12 +15,14 @@
         @endsession
 
         @if ($result)
+            <h1 class="text-2xl mb-3 text-white">Result</h1>
             <h2 class="text-yellow-300 mb-2">Save it or you will never get the link again 😁</h2>
             <img class="mb-6 rounded-lg overflow-hidden" src="{{ $result }}">
         @endif
 
         <div class="w-full mb-4 flex flex-col">
-            <label class="label-text mb-1" for="image">Pick an image file (2024KB Max)</label>
+            <label class="label-text mb-1" for="image">Pick an image file (<span class="text-red-500">*</span> 2024KB
+                Max)</label>
             <input type="file" id="image" wire:model="image"
                 class="file-input file-input-bordered file-input-info w-full max-w-xs" accept="image/*" required />
             @error('image')
@@ -23,13 +31,16 @@
         </div>
 
         <div class="w-full mb-4 flex flex-col">
-            <label class="label-text mb-1" for="image">Pick your watermark (2024KB Max)</label>
+            <label class="label-text mb-1" for="image">Pick your watermark (<span class="text-red-500">*</span>
+                2024KB
+                Max)</label>
             <input type="file" id="image" wire:model="watermark"
                 class="file-input file-input-bordered file-input-info w-full max-w-xs" accept="image/*" required />
             @error('watermark')
                 <p class="text-red-600 py-2">{{ $message }}</p>
             @enderror
         </div>
+        <p class="text-yellow-300 mb-3">Final Result can be different from the preview</p>
         <button type="submit" class="btn btn-block mb-3" wire:loading.remove wire:target="addWatermark">Start
             processing</button>
         <button class="btn" wire:loading wire:target="addWatermark" disabled>
