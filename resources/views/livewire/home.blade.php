5c73c6a9c9cafe2cd27bf069b543f1fb340e7094
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-09-22 16:16:04 +0500

    path fix

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -8,28 +8,32 @@
             <x-error message="{{ $value }}" />
         @endsession
 
-        <div class="w-full mb-4 flex flex-col">
-            <label class="label-text mb-1" for="image">Pick an image file</label>
-            <input type="file" id="image" wire:model="image"
-                class="file-input file-input-bordered file-input-info w-full max-w-xs" accept="image/*" required />
-            @error('image')
-                <p class="text-red-600 py-2">{{ $message }}</p>
-            @enderror
-        </div>
+        @if ($result)
+            <p class="text-white"></p>
+            @endsession
 
-        <div class="w-full mb-4 flex flex-col">
-            <label class="label-text mb-1" for="image">Pick your watermark</label>
-            <input type="file" id="image" wire:model="watermark"
-                class="file-input file-input-bordered file-input-info w-full max-w-xs" accept="image/*" required />
-            @error('watermark')
-                <p class="text-red-600 py-2">{{ $message }}</p>
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-block mb-3" wire:loading.remove wire:target="addWatermark">Start
-            processing</button>
-        <button class="btn" wire:loading wire:target="addWatermark" disabled>
-            <span class="loading loading-spinner"></span>
-            loading
-        </button>
+            <div class="w-full mb-4 flex flex-col">
+                <label class="label-text mb-1" for="image">Pick an image file</label>
+                <input type="file" id="image" wire:model="image"
+                    class="file-input file-input-bordered file-input-info w-full max-w-xs" accept="image/*" required />
+                @error('image')
+                    <p class="text-red-600 py-2">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="w-full mb-4 flex flex-col">
+                <label class="label-text mb-1" for="image">Pick your watermark</label>
+                <input type="file" id="image" wire:model="watermark"
+                    class="file-input file-input-bordered file-input-info w-full max-w-xs" accept="image/*" required />
+                @error('watermark')
+                    <p class="text-red-600 py-2">{{ $message }}</p>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-block mb-3" wire:loading.remove wire:target="addWatermark">Start
+                processing</button>
+            <button class="btn" wire:loading wire:target="addWatermark" disabled>
+                <span class="loading loading-spinner"></span>
+                loading
+            </button>
     </form>
 </section>
