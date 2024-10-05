50deb4cd6b69a8590a9e2de5aaa022b6a9be49a2
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-10-05 22:14:32 +0500

    Code -3

--- app/Livewire/Remover.php
+++ app/Livewire/Remover.php
@@ -63,15 +63,13 @@ class Remover extends Component
             foreach ($this->images as $singleImage) {
                 $imagePath = $singleImage->store('bg_removed');
                 $img = new \Imagick(storage_path('app/private/' . $imagePath));
-                $img->setImageFormat('png');
                 $cornerColor = $img->getImagePixelColor(0, 0)->getColor();
                 $cornerColorString = sprintf('rgb(%d,%d,%d)', $cornerColor['r'], $cornerColor['g'], $cornerColor['b']);
                 $fuzz = 0.1 * \Imagick::getQuantumRange()['quantumRangeLong'];
-                $img->setImageFuzz($fuzz);
                 $img->transparentPaintImage($cornerColorString, 0.0, $fuzz, false);
                 $img->setImageAlphaChannel(\Imagick::ALPHACHANNEL_ACTIVATE);
                 $img->setBackgroundColor(new \ImagickPixel('transparent'));
-                $bgremovedImage = 'bg_removed/bg-removed-' . rand(9, 999999) . '-' . pathinfo($singleImage->getClientOriginalName(), PATHINFO_FILENAME) . '.png';
+                $bgremovedImage = 'bg_removed/bg-removed-' . rand(9, 999999) . '-' . pathinfo($singleImage->getClientOriginalName(), PATHINFO_FILENAME) . '.' . $singleImage->getClientOriginalExtension();
                 $img->writeImage(storage_path('app/private/' . $bgremovedImage));
                 $img->clear();
                 $img->destroy();
@@ -84,7 +82,6 @@ class Remover extends Component
                     'url' => config('app.url') . '/storage/' . $bgremovedImage
                 ];
             }
-
             session()->flash('success', 'Background removed from all images!');
         } catch (\Exception $e) {
             Log::error('BGRemoval error: ' . $e->getMessage());
